b05be2097abb307ae8a4ebfcea2a1ed9d6a3323e
Jason <jason.nathan.chang@gmail.com>
2025-08-20 17:46:33 +0800

    Added scanner elements and while loop

--- src/main/java/JohnChatbot.java
+++ src/main/java/JohnChatbot.java
@@ -1,8 +1,18 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class JohnChatbot {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String goodbye = "Farewell. I look forward to our next meeting.";
         System.out.println("Hello. I am John Chatbot.\n"
-        + "How can I help you?\n"
-        + "\n"
-        + "Farewell. I look forward to our next meeting.");
+                + "How can I help you?\n"
+                + "\n");
+        String text = input.nextLine();
+        while (!Objects.equals(text, "bye")) {
+            System.out.println(text);
+            text = input.nextLine();
+        }
+
     }
 }
