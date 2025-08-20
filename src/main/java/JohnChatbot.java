8ed31972511eb08b36c248427b39ae9a0e2a4c8f
Jason <jason.nathan.chang@gmail.com>
2025-08-20 17:33:04 +0800

    Changed name and greeting

---
+++ src/main/java/JohnChatbot.java
@@ -0,0 +1,8 @@
+public class JohnChatbot {
+    public static void main(String[] args) {
+        System.out.println("Hello. I am John Chatbot.\n"
+        + "How can I help you?\n"
+        + "\n"
+        + "Farewell. I look forward to our next meeting.");
+    }
+}
